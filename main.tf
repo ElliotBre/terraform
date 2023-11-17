b50b9600fba2a9fb274e0d339aca0b286eaea13e
ElliotBre <124515858+ElliotBre@users.noreply.github.com>
2023-11-17 10:38:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,23 +3,6 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.instance_region
 }
-
-resource "aws_security_group" "security-group1" {
-  name = "security-group1"
-  ingress {
-    description = "Allows ssh in"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress{
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 resource "aws_vpc" "very_cool_vpc"{
     cidr_block = "10.0.0.0/16"
     enable_dns_support = "true"
