741b2f1d78b786c448bd725ca5cab771077e6df2
ElliotBre <124515858+ElliotBre@users.noreply.github.com>
2023-11-17 10:37:48 +0000

    Update network.tf

--- network.tf
+++ network.tf
@@ -19,6 +19,22 @@ resource "aws_route_table_association" "very_cool_association" {
     subnet_id = aws_subnet.subnetpub.id
     route_table_id = aws_route_table.very_cool_table.id
 }
+resource "aws_security_group" "security-group1" {
+  name = "security-group1"
+  ingress {
+    description = "Allows ssh in"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
